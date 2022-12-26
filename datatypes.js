3d66a3b1760587993f6835e597f557c5aae0729d
ashish <ashmangl@publicisgroupe.net>
2022-12-26 18:39:27 +0530

    add new code

--- datatypes.js
+++ datatypes.js
@@ -1,11 +1,41 @@
 /*
 String
 */
-var Fname;
+let Fname;
 Fname = "ashish";
 console.log(Fname);
-var Lname;
+let Lname;
 Lname = 'Mangla';
 console.log(Lname);
-var LnameUpper = Lname.toUpperCase();
+let LnameUpper = Lname.toUpperCase();
 console.log(LnameUpper);
+/*
+Number
+*/
+let age;
+age = 25;
+age = 22.5;
+let dob = "25";
+let result = parseInt(dob);
+/**
+ Boolean
+ */
+let isVald;
+console.log(isVald);
+/*
+Array
+*/
+let empList;
+empList = ["ashish", "mangla", "ashi"];
+console.log(empList);
+let numList;
+numList = [1, 2, 3, 4, 5];
+console.log(numList);
+let numResult = numList.filter((num) => num > 2);
+console.log(numResult);
+let numFind = numList.find((num) => num === 2);
+console.log(numFind);
+let empFind = empList.find((emp) => emp === 'ashish');
+console.log(empFind);
+let sum = numList.reduce((acc, num) => acc + num);
+console.log(sum);
--- datatypes.ts
+++ datatypes.ts
@@ -1,7 +1,7 @@
 /* 
 String
 */
-let Fname: string ;
+let Fname: string;
 Fname = "ashish";
 console.log(Fname);
 
@@ -11,3 +11,51 @@ console.log(Lname);
 
 let LnameUpper = Lname.toUpperCase();
 console.log(LnameUpper);
+
+
+/*
+Number
+*/
+
+let age: number;
+age = 25;
+age = 22.5;
+
+let dob = "25";
+
+let result = parseInt(dob);
+
+/**
+ Boolean
+ */
+
+ let isVald : boolean;
+ console.log(isVald);
+
+
+ /*
+ Array
+ */
+let empList : string[];
+
+empList = ["ashish","mangla","ashi"];
+console.log(empList);
+
+let numList : Array<number>;
+numList = [1,2,3,4,5]
+console.log(numList);
+
+let numResult  = numList.filter((num) => num > 2);
+console.log(numResult);
+
+let numFind =  numList.find((num) => num === 2);
+console.log(numFind);
+
+let empFind =  empList.find((emp) => emp === 'ashish');
+console.log(empFind);
+
+let sum = numList.reduce((acc,num) => acc+ num);
+console.log(sum);
+
+
+
